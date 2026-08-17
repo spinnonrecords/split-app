8009bb4d813ac1c229188f7104ac698c27bc8248
spinnonrecords <spinnonrec@gmail.com>
2026-08-17 13:54:51 +0300

    Fix next.config.ts duplicate declaration

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   typescript: {
-    // מתעלם משגיאות טייפסקריפט בזמן ההעלאה לשרת
     ignoreBuildErrors: true,
   },
   eslint: {
-    // מתעלם משגיאות ESLint בזמן ההעלאה לשרת
     ignoreDuringBuilds: true,
   },
 };
